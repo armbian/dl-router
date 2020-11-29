3f5d75e43e695e40a80cf039523498b5c8136efe
Lane Jennison <lane@lane-fu.com>
2020-11-29 13:53:27 -0500

    uwsgi safe reload adn yaml based config.. wip region

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
+""" flask app to redirect request to appropriate armbian mirror and image """
+
+import uwsgi
+import json
+
 from flask import (
         Flask,
         redirect,
@@ -6,22 +11,45 @@ from flask import (
 # from markupsafe import escape
 from download_image_map import Parser
 from mirror_list import Mirror
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import (
+    FoldedScalarString,
+    LiteralScalarString,
+)
 
 
 def load_mirrors():
-    with open('mirrors.conf', 'r') as f:
-        all_mirrors = f.read().splitlines()
-        return all_mirrors
+    """ open mirrors file and return contents """
+#    with open('mirrors.conf', 'r') as row:
+#        all_mirrors=row.read().splitlines()
+    yaml = YAML()
+    yaml.indent(mapping=2, sequence=4, offset=2)
+    yaml.preserve_quotes = True
+
+    with open('mirrors.yaml', 'r') as f:
+        config = yaml.load(f)
+    return config['apt']
+
+
+def reload_all():
+    """ reload mirror and redirect map files """
+    load_mirrors()
+    global mirror
+    mirror = Mirror(load_mirrors())
+    global dl_map
+    dl_map = parser.reload()
+    return dl_map
 
 
 def get_ip():
+    """ returns requestor's IP by parsersing proxy  headers """
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         return request.environ['REMOTE_ADDR']
-    else:
-        return request.environ['HTTP_X_FORWARDED_FOR']
+    return request.environ['HTTP_X_FORWARDED_FOR']
 
 
-def get_redirect(path, IP):
+def get_redirect(path, IP, region=None):
+    """ get redirect based on path and IP(future) """
     split_path = path.split('/')
     if len(split_path) == 2:
         key = "{}/{}".format(split_path[0], split_path[1])
@@ -29,9 +57,8 @@ def get_redirect(path, IP):
         return "{}{}".format(mirror.next(), new_path)
     if path == '':
         return mirror.next()
-    else:
-        print("path: {}".format(path))
-        return "{}{}".format(mirror.next(), path)
+    print("path: {}".format(path))
+    return "{}{}".format(mirror.next(), path)
 
 
 mirror = Mirror(load_mirrors())
@@ -42,23 +69,27 @@ dl_map = parser.parsed_data
 app = Flask(__name__)
 
 
-@app.route('/status')
+@ app.route('/status')
 def status():
+    """ return health check status """
     return "OK"
 
 
-@app.route('/reload')
-def reload():
-    load_mirrors()
-    global mirror
-    mirror = Mirror(load_mirrors())
-    global dl_map
-    dl_map = parser.reload()
+@ app.route('/reload')
+def signal_reload():
+    """ trigger graceful reload via uWSGI """
+    uwsgi.reload()
     return dl_map
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@ app.route('/mirrors')
+def show_mirrors():
+    """ return all_mirrors in json format to requestor """
+    return json.dumps(mirror.all_mirrors())
+
+
+@ app.route('/', defaults={'path': ''})
+@ app.route('/<path:path>')
 def catch_all(path):
     return redirect(get_redirect(path, get_ip()), 302)
 
--- app/mirror_list.py
+++ app/mirror_list.py
@@ -1,16 +1,35 @@
-#!/usr/bin/env python
+""" manage iterating through mirrors and return appropriate one based by region """
+
+
 class Mirror():
-    def __init__(self, mirror_list=None):
+
+    def __init__(self, mirror_list):
+        self.mirror_list = dict()
+        self._list_position = dict()
+        self._list_max = dict()
+        self.mirror_list['default'] = list(
+            mirror_list['NA'] + mirror_list['EU'])
         self.mirror_list = mirror_list
-        self._list_position = 0
-        self._list_max = len(mirror_list) - 1
+        for region in list(mirror_list.keys()):
+            self._list_position[region] = 0
+            self._list_max[region] = len(mirror_list[region]) - 1
 
-    def increment(self):
-        if self._list_position == self._list_max:
-            self._list_position = 0
+    def increment(self, region='default'):
+        """ move to next element regions mirror list and return list position"""
+        if self._list_position[region] == self._list_max[region]:
+            self._list_position[region] = 0
         else:
-            self._list_position = self._list_position + 1
+            self._list_position[region] = self._list_position[region] + 1
 
-    def next(self):
-        self.increment()
-        return self.mirror_list[self._list_position]
+    def next(self, region='default'):
+        """ return next mirror in rotation """
+        self.increment(region)
+        return self.mirror_list[region][self._list_position[region]]
+
+    def all_mirrors(self):
+        """ return all mirrrors configured """
+        self.mirror_list
+
+    def all_regions(self):
+        """ return list of regions configured """
+        return list(self.mirror_list.keys())
